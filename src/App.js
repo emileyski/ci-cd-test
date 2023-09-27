b724a0ff0fa66e54ec59b3905320c3c5e20a1473
emileyski <96009930+emileyski@users.noreply.github.com>
2023-09-27 14:43:28 +0300

    added pipeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import {useState} from "react";
 
 function App() {
-  return <div>Hi there</div>;
+    const [state, setState] = useState(0)
+
+    const increment = () => setState(prev => prev + 1);
+
+    return (
+        <div className="App">
+            <h1 style={{fontSize: 70, color: 'red'}}>value = {state}</h1>
+            <button
+                style={{background: 'black', color: "white", padding: 15}}
+                onClick={increment}
+            >
+                incremernt
+            </button>
+        </div>
+    );
 }
 
 export default App;
